e87069761a4eb19ef4c13bee06ca7412a3364f6c
Dominik Fuchß <develop@fuchss.org>
2023-07-03 21:22:26 +0200

    Fix naming of variables

--- src/main/kotlin/org/fuchss/matrix/joinlink/Crypto.kt
+++ src/main/kotlin/org/fuchss/matrix/joinlink/Crypto.kt
@@ -14,7 +14,7 @@ import kotlin.io.encoding.ExperimentalEncodingApi
 
 private const val ENCRYPTION_ALGORITHM = "AES"
 private const val KEY_ALGORITHM = "PBKDF2WithHmacSHA256"
-private const val CIPHER_NAME = "AES/GCM/NoPadding"
+private const val TRANSFORMATION_NAME = "AES/GCM/NoPadding"
 private val logger = LoggerFactory.getLogger("org.fuchss.matrix.joinlink.Crypto")
 
 fun RoomId.encrypt(config: Config): String = encryptRoomId(config, this)
@@ -27,16 +27,15 @@ private fun encryptRoomId(config: Config, roomId: RoomId): String {
     random.nextBytes(salt)
 
     val factory = SecretKeyFactory.getInstance(KEY_ALGORITHM)
-    val pbeKeySpec: KeySpec = PBEKeySpec(config.encryptionKey.toCharArray(), salt, 1000, 256)
-    val secretKey = factory.generateSecret(pbeKeySpec)
-    val secret = SecretKeySpec(secretKey.encoded, ENCRYPTION_ALGORITHM)
+    val keySpec: KeySpec = PBEKeySpec(config.encryptionKey.toCharArray(), salt, 1000, 256)
+    val secretKey = factory.generateSecret(keySpec)
+    val secretKeySpec = SecretKeySpec(secretKey.encoded, ENCRYPTION_ALGORITHM)
 
-    val cipher = Cipher.getInstance(CIPHER_NAME)
+    val cipher = Cipher.getInstance(TRANSFORMATION_NAME)
     val iv = ByteArray(cipher.blockSize)
     random.nextBytes(iv)
 
-    val gcmParameterSpec = GCMParameterSpec(128, iv)
-    cipher.init(Cipher.ENCRYPT_MODE, secret, gcmParameterSpec)
+    cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, GCMParameterSpec(128, iv))
 
     val encrypted = cipher.doFinal(roomId.full.toByteArray())
     return "${Base64.encode(salt)}|${Base64.encode(iv)}|${Base64.encode(encrypted)}"
@@ -52,12 +51,12 @@ private fun decryptRoomId(config: Config, content: String?): RoomId? {
         val (salt, iv, encryptedContent) = content.split("|").map { Base64.decode(it) }
 
         val factory = SecretKeyFactory.getInstance(KEY_ALGORITHM)
-        val pbeKeySpec: KeySpec = PBEKeySpec(config.encryptionKey.toCharArray(), salt, 1000, 256)
-        val secretKey = factory.generateSecret(pbeKeySpec)
-        val secret = SecretKeySpec(secretKey.encoded, ENCRYPTION_ALGORITHM)
+        val keySpec: KeySpec = PBEKeySpec(config.encryptionKey.toCharArray(), salt, 1000, 256)
+        val secretKey = factory.generateSecret(keySpec)
+        val secretKeySpec = SecretKeySpec(secretKey.encoded, ENCRYPTION_ALGORITHM)
 
-        val cipher = Cipher.getInstance(CIPHER_NAME)
-        cipher.init(Cipher.DECRYPT_MODE, secret, GCMParameterSpec(128, iv))
+        val cipher = Cipher.getInstance(TRANSFORMATION_NAME)
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, GCMParameterSpec(128, iv))
 
         val decrypted = cipher.doFinal(encryptedContent)
         RoomId(String(decrypted))
