0c4d33d4e2da9c7e35ae2c027fc077c1a7b5498d
Dominik Fuchß <develop@fuchss.org>
2023-08-27 14:57:31 +0200

    Cleanup code

--- src/main/kotlin/org/fuchss/matrix/joinlink/events/JoinLinkEventContent.kt
+++ src/main/kotlin/org/fuchss/matrix/joinlink/events/JoinLinkEventContent.kt
@@ -2,14 +2,8 @@ package org.fuchss.matrix.joinlink.events
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import net.folivo.trixnity.core.model.RoomId
 import net.folivo.trixnity.core.model.events.EventType
 import net.folivo.trixnity.core.model.events.StateEventContent
-import org.fuchss.matrix.joinlink.Config
-import javax.crypto.Cipher
-import javax.crypto.spec.SecretKeySpec
-import kotlin.io.encoding.Base64
-import kotlin.io.encoding.ExperimentalEncodingApi
 
 /**
  * The content of a joinlink event.
@@ -21,38 +15,5 @@ data class JoinLinkEventContent(
 ) : StateEventContent {
     companion object {
         val ID = EventType(JoinLinkEventContent::class, "org.fuchss.matrix.joinlink")
-
-        private const val ENCRYPTION_ALGORITHM = "AES/GCM/NoPadding"
-        private const val KEY_ALGORITHM = "AES"
-
-        @OptIn(ExperimentalEncodingApi::class)
-        fun create(config: Config, roomId: RoomId?): JoinLinkEventContent {
-            if (roomId == null) {
-                return JoinLinkEventContent(null)
-            }
-
-            val secretKey = SecretKeySpec(config.encryptionKey.toByteArray(), KEY_ALGORITHM)
-            val cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM)
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey)
-
-            val encrypted = cipher.doFinal(roomId.full.toByteArray())
-            val encodedEncryptedRoom = Base64.encode(encrypted)
-            return JoinLinkEventContent(encodedEncryptedRoom)
-        }
-
-        @OptIn(ExperimentalEncodingApi::class)
-        fun decrypt(config: Config, content: JoinLinkEventContent): RoomId? {
-            if (content.joinlinkRoom == null) {
-                return null
-            }
-
-            val secretKey = SecretKeySpec(config.encryptionKey.toByteArray(), KEY_ALGORITHM)
-            val cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM)
-            cipher.init(Cipher.DECRYPT_MODE, secretKey)
-
-            val decodedEncryptedRoom = Base64.decode(content.joinlinkRoom)
-            val decrypted = cipher.doFinal(decodedEncryptedRoom)
-            return RoomId(String(decrypted))
-        }
     }
 }
